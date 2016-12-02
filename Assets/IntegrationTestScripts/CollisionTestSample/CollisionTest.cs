cd0ea42abb5cf9927ec63a4412693824cf27abb8
Shunta Nomura <nomura@kayac.vn>
2016-12-02 11:15:30 +0700

    add simple collision test

---
+++ Assets/IntegrationTestScripts/CollisionTestSample/CollisionTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour {
+
+
+    public void OnCollisionEnter()
+    {
+        IntegrationTest.Pass();
+    }
+}
